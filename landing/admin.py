1a22d0547ecc5b5c50f00fdc516423f887baa900
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2017-11-29 21:58:22 +0600

    Setting up djangoadmin for model of subscribers

--- landing/admin.py
+++ landing/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email']
+    list_filter = ['name', 'email']
+    search_fields = ['name', 'email']
+
+    class Meta:
+        model = Subscriber
+
+
+
+admin.site.register(Subscriber, SubscriberAdmin)
--- landing/models.py
+++ landing/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class Subscriber(models.Model):
     email = models.EmailField()
     name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name 
+
+    class Meta:
+        verbose_name = 'Подписчик'
+        verbose_name_plural = 'Подписчики'
