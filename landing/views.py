48e44bbe337ad9bfbc21c75efd82740f49ea36d6
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2017-11-29 20:49:28 +0600

    Subscriber Form

--- landing/views.py
+++ landing/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .forms import *
 
 
 def landing(request):
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+
+        data = form.cleaned_data
+        print(data['name'])
+
+        form.save()
+
+
     return render(request, 'landing/landing.html', locals())
