5d40acd20d840e8f00ea6da3d2bb5596bd58e571
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2017-12-05 21:19:36 +0600

    Save and post_save

--- products/models.py
+++ products/models.py
@@ -3,13 +3,14 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=128, blank=True, null=True, default=None)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     description = models.TextField(blank=True, default=None)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     update = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return '%s' % self.name
+        return '%s - %s' % (self.name, self.price)
 
     class Meta:
         verbose_name = 'Товар'
