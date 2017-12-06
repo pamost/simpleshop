308a7fc74dabd21c0003b31d188443a733abec3c
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2017-12-06 23:06:57 +0600

    Fix urls

--- landing/urls.py
+++ landing/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from landing import views
 
 urlpatterns = [
-    url(r'^', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
     url(r'^landing/', views.landing, name='landing'),
 ]
