6d7e786d2fca5b51ddaedf136544a38f94bc495b
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2017-12-13 00:06:24 +0600

    App js card

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
-    var form = $('#form_buy_product')
+    var form = $('#form_buy_product');
     console.log(form);
+
+    form.on('submit', function (e) {
+        e.preventDefault();
+        var number = $('#number').val();
+        var submit_button = $('#submit_button');
+        var product_id = submit_button.data("product_id");
+        var product_name = submit_button.data("product_name");
+        var product_price= submit_button.data("product_price");
+        console.log(number, product_id, product_name, product_price);
+    })
 });
